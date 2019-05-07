871a83b3544e2b8061211979af4138cbb3f5f68b
CERIO18 <ceurioste@derp.org>
2019-05-07 14:21:14 -0600

    please scraper work

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -1,17 +1,19 @@
 var request = require("request");
 var cheerio = require("cheerio");
 
-var scrape = function (cb) {
-    request("https://www.nytimes.com", function(err, res, body) {
-
-    var $ = cheerio.load(body);
-
-    var articles = [];
+var scrape = function() {
+    // Scrape the NYTimes website
+    return axios.get("http://www.nytimes.com").then(function(res) {
+      var $ = cheerio.load(res.data);
+      console.log("scraping");
+      // Make an empty array to save our article info
+      var articles = [];
 
     $("div.css-1100km").each(function(i, element){
 
-        var head = $(this).children("a.article-title").text().trim();
-        var sum = $(this).children("href").text().trim();
+        var head = $(this).find("h2").text().trim();
+        var url = $(this).find("a").attr("href");
+        var sum = $(this).find("p").text().trim();
         
         if (head && sum) {
             var headNeat = head.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
@@ -19,14 +21,15 @@ var scrape = function (cb) {
 
             var dataToAdd = {
                 headline: headNeat,
-                summary: sumNeat
+                summary: sumNeat,
+                url: "https://www.nytimes.com" + url
             };
 
             articles.push(dataToAdd); 
             console.log(articles)
         }
     })
-    cb(articles);
+    return(articles);
 });
 };
 module.exports = scrape;
